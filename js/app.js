619922dc3dcb1e996b64c1927729a654024f07b3
Omar Jesus Carrasco Guerrero <omarcarrascoguerrero@gmail.com>
2023-03-30 00:15:36 -0500

    segundo commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,32 @@ const button = document.querySelector('.button');
 const footer = document.querySelector('.footer');
 const sopla = document.querySelector('.sopla');
 const pastel = document.querySelector('.pastel-active');
+const buttonFooter = document.querySelector('.footer__button');
+const main = document.querySelector('.main');
+const reglos = document.querySelector('.regalos');
+
+const typed = new Typed('.typed', {
+    strings : ['<i class="palbra-main">Perra</i', '<i class="palbra-main">Sucia</i', '<i class="palbra-main">Mongola</i'],
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 2000,
+    loop: true,
+    contentType: 'html'
+
+});
+
+// const gege = new Typed('.main-maquina', {
+//     strings: ['Esto es para la chica mas fea, osea la GLENDI'],
+//     typeSpeed: 75,
+//     startDelay: 300,
+//     backSpeed: 75,
+//     backDelay: 2000,
+//     contentType: 'html'
+
+// });
+
+console.log(buttonFooter)
 
 setTimeout(function() {
     const message = document.getElementById('result');
@@ -18,6 +44,10 @@ button.addEventListener('click', ()=>{
     footer.classList.add('footer-active')
 })
 
-sopla.addEventListener('click', ()=>{
-    pastel.style.display = "none";
+
+buttonFooter.addEventListener('click', ()=>{
+    footer.classList.remove('footer-active')
+    main.style.display = "none";
+    reglos.style.display = "initial";
+    console.log('omar')
 })
