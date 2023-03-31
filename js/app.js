d4d2e542adef8f0a6ca2bc82fc876f1c048f379f
Omar Jesus Carrasco Guerrero <omarcarrascoguerrero@gmail.com>
2023-03-30 23:18:36 -0500

    tree commit

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const main = document.querySelector('.main');
 const reglos = document.querySelector('.regalos');
 
 const typed = new Typed('.typed', {
-    strings : ['<i class="palbra-main">Perra</i', '<i class="palbra-main">Sucia</i', '<i class="palbra-main">Mongola</i'],
+    strings: ['<i class="palbra-main">Perra</i', '<i class="palbra-main">Sucia</i', '<i class="palbra-main">Mongola</i'],
     typeSpeed: 75,
     startDelay: 300,
     backSpeed: 75,
@@ -17,35 +17,49 @@ const typed = new Typed('.typed', {
 
 });
 
-// const gege = new Typed('.main-maquina', {
-//     strings: ['Esto es para la chica mas fea, osea la GLENDI'],
-//     typeSpeed: 75,
-//     startDelay: 300,
-//     backSpeed: 75,
-//     backDelay: 2000,
-//     contentType: 'html'
 
-// });
+function escribirEnPantalla(texto) {
+    let arr = texto.split("");
+    i = 0;
+    let intervalo = setInterval(function () {
+        if (i == arr.length - 1) {
+            document.querySelector(".main-maquina").innerHTML += arr[i];
+            clearInterval(intervalo);
+        }
+        else {
+            if (arr[i] == " ") {
+                document.querySelector(".main-maquina").innerHTML += arr[i];
+                document.querySelector(".main-maquina").innerHTML += arr[i + 1];
+                i += 2;
+            } else {
+                document.querySelector(".main-maquina").innerHTML += arr[i];
+                i++;
+            }
+        }
+    }, 90);
+}
+
+escribirEnPantalla("Esto es para la chica mas fea, o sea la GLENDI");
 
 console.log(buttonFooter)
 
-setTimeout(function() {
+setTimeout(function () {
     const message = document.getElementById('result');
     message.innerHTML = "Espero te guste y si no andate a la mrd"
     button.style.display = "initial";
 }, 6200);
-setTimeout(function() {
+setTimeout(function () {
     const message = document.getElementById('state-message');
     message.style.display = "none";
 }, 6000);
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     footer.style.display = "initial";
     footer.classList.add('footer-active')
 })
 
 
-buttonFooter.addEventListener('click', ()=>{
+buttonFooter.addEventListener('click', () => {
     footer.classList.remove('footer-active')
     main.style.display = "none";
     reglos.style.display = "initial";
